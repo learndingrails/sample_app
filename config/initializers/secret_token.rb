e6af68ef566dcae068dc7445286c633d63f86dd2
Tristan <tristan@dynamicclarity.com>
2010-11-19 12:29:37 +1100

    initial commit, booha

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'da9013f029f031e938aec73d14fd5a9dde07047d7fd9baa7fa40ed9495ac78c3e85d19cfce37d20bcd214af0c645c756321fe9923abce88f6e642d0fb1e38c05'
